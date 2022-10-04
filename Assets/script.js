a0e3cb4f5a1f0fad885a11300176682186631b00
unknown <jimmymhuang@gmail.com>
2022-10-03 22:00:38 -0500

    edits to HTML and CSS files

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,15 @@
-function setTime(){
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft + " seconds left.";
-    
-        if(secondsLeft === 0) {
-          clearInterval(timerInterval);
-          sendMessage();
-        }
-    
-      }, 1000);
-    }
-}
+//timer
+//function timer(){
+//}
+//separate function for time penalty for wrong answers?
+
+//event listener for start button
+//event listener for answer choice buttons 
+    //right answer moves onto next question and answers, "correct" text below
+    //wrong answer incurs time penalty, moves onto next q, "wrong" text below
+//score screen when time runs out
+//text box for entering initials: 
+//"go back" button: redirects to game start page
+//"clear high scores" button for clearing local storage
+
+//High scores: save locally, list initials and scores
